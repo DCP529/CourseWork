d16320716d45b419fa10ad306955dbd8e24f79e4
tima.kolesnik.2015@mail.ru <tima.kolesnik.2015@mail.ru>
2021-05-15 23:31:11 +0300

    first commit

---
+++ Delegate/Factory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate string CompareItems(Factory obj1, Factory obj2);
+    public class Factory
+    {
+        string name;
+        decimal salary;
+        int numberWorkers;
+        double amountEmissions;
+
+        public Factory(string name, decimal salary, int numberWorkers, double amountEmissions)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.numberWorkers = numberWorkers;
+            this.amountEmissions = amountEmissions;
+        }
+
+        static string CompareSalary(Factory factory1, Factory factory2)
+        {
+            if (factory1.salary < factory2.salary)
+            {
+                return factory2.name;
+            }
+            else if (factory1.salary > factory2.salary)
+            {
+                return factory1.name;
+            }
+            else
+            {
+                return "Они одинаковы";
+            }
+        }
+
+        static string CompareNumberWorkers(Factory factory1, Factory factory2)
+        {
+            if (factory1.salary < factory2.salary)
+            {
+                return factory2.name;
+            }
+            else if (factory1.salary > factory2.salary)
+            {
+                return factory1.name;
+            }
+            else
+            {
+                return "Они одинаковы";
+            }
+        }
+
+        static string CompareAmountEmissions(Factory factory1, Factory factory2)
+        {
+            if (factory1.salary < factory2.salary)
+            {
+                return factory2.name;
+            }
+            else if (factory1.salary > factory2.salary)
+            {
+                return factory1.name;
+            }
+            else
+            {
+                return "Они одинаковы";
+            }
+        }
+
+        public static CompareItems SortCompareSalary
+        {
+            get { return (new CompareItems(CompareSalary)); }
+        }
+        public static CompareItems SortCompareNumberWorkers
+        {
+            get { return (new CompareItems(CompareNumberWorkers)); }
+        }
+        public static CompareItems SortCompareAmountEmissions
+        {
+            get { return (new CompareItems(CompareAmountEmissions)); }
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory1 = new Factory("Краснодарская ТЭС", 2000.25m, 3700, 156187);
+            Factory factory2 = new Factory("Краснодарская ГЭС", 1564m, 3915, 1564);
+
+            Console.WriteLine("Выберите один из 3-х критериев сортировки предприятий данного региона:" +
+                "\n1)З/п\n2)Количество рабочих\n3)Величина выбросов в атмосферу\n");
+
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            switch (result)
+            {
+                case 1:
+                    Console.WriteLine(Factory.SortCompareSalary(factory1, factory2));
+                    break;
+                case 2:
+                    Console.WriteLine(Factory.SortCompareNumberWorkers(factory1, factory2));
+                    break;
+                case 3:
+                    Console.WriteLine(Factory.SortCompareAmountEmissions(factory1, factory2));
+                    break;
+                default:
+                    Console.WriteLine("Такого критерия сортировки нет!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
